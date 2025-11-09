0918a64a3156db43200c3998c3ada732b65fb633
dx3mod <dx3mod@bk.ru>
2025-11-09 15:14:11 +0300

    Fix memcpy type casting

--- src/crypt_stubs.c
+++ src/crypt_stubs.c
@@ -5,25 +5,27 @@
 
 #include <caml/mlvalues.h>
 
-CAMLprim value ml_crypt(value buf, value key, value salt) {
+CAMLprim value ml_crypt(value buf, value key, value salt)
+{
   int pwlen;
   char *password = crypt(String_val(key), String_val(salt));
 
   if (password == NULL)
-      return Val_int(-1);
+    return Val_int(-1);
 
   pwlen = strlen(password);
-  memcpy(String_val(buf), password, pwlen);
+  memcpy((void *)String_val(buf), password, pwlen);
   return Val_int(pwlen);
 }
 
 // http://www.gnu.org/software/libc/manual/html_node/crypt.html
-value ml_crypt_md5(value buf, value key) {
+value ml_crypt_md5(value buf, value key)
+{
   unsigned long seed[2];
   char salt[] = "$1$........";
   const char *const seedchars =
-    "./0123456789ABCDEFGHIJKLMNOPQRST"
-    "UVWXYZabcdefghijklmnopqrstuvwxyz";
+      "./0123456789ABCDEFGHIJKLMNOPQRST"
+      "UVWXYZabcdefghijklmnopqrstuvwxyz";
   char *password;
   int i, pwlen;
 
@@ -34,13 +36,13 @@ value ml_crypt_md5(value buf, value key) {
 
   /* Turn it into printable characters from `seedchars'. */
   for (i = 0; i < 8; i++)
-    salt[3+i] = seedchars[(seed[i/5] >> (i%5)*6) & 0x3f];
+    salt[3 + i] = seedchars[(seed[i / 5] >> (i % 5) * 6) & 0x3f];
 
   /* Read in the user's password and encrypt it. */
   password = crypt(String_val(key), salt);
   pwlen = strlen(password);
 
   /* Print the results. */
-  memcpy(String_val(buf), password, pwlen);
+  memcpy((void *)String_val(buf), password, pwlen);
   return Val_int(pwlen);
 }
