e21b4ba5a39a4d98697ee1f7a5b9160443d6d899
dx3mod <dx3mod@bk.ru>
2025-11-09 20:48:47 +0300

    Add Salt module

--- src/crypt.ml
+++ src/crypt.ml
@@ -8,9 +8,13 @@ let pp_key_derivation fmt derivation =
   Format.pp_print_string fmt
   @@ match derivation with Md5 -> "1" | Sha256 -> "5" | Sha512 -> "6"
 
-let crypt ?derivation ~salt key =
+let crypt ?derivation ?salt key =
+  let salt = match salt with None -> Salt.gen_base64 9 | Some salt -> salt in
+
   match derivation with
   | None -> Ffi.crypt key salt
   | Some derivation ->
       Format.asprintf "$%a$%s" pp_key_derivation derivation salt
       |> Ffi.crypt key
+
+module Salt = Salt
--- src/crypt.mli
+++ src/crypt.mli
@@ -5,15 +5,18 @@ type key_derivation =
   | Sha256
   | Sha512  (** The glibc2's additional encryption algorithms. *)
 
-val crypt : ?derivation:key_derivation -> salt:string -> string -> string
+val crypt : ?derivation:key_derivation -> ?salt:string -> string -> string
 (** [crypt ?derivation ~salt key] is the password encryption function. It is
     based on the Data Encryption Standard algorithm with variations intended
     (among other things) to discourage use of hardware implementations of a key
     search.
 
-    @param salt salt is a two-character string chosen from the set [a-zA-Z0-9./]
-    @param key key is a user's typed password
+    @param salt
+      Salt is a two-character string chosen from the set [a-zA-Z0-9./].
+    @param key Key is a user's typed password.
 
-    @return encrypted password
+    @return Encrypted password.
 
-    @raise Failure if hashing fails *)
+    @raise Failure If hashing fails. *)
+
+module Salt = Salt
